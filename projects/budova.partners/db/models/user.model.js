f814928e5be1546bf517078780343b650c0134bd
Siteodessa <siteodessa.com@gmail.com>
2018-08-21 20:34:02 +0300

    v0.0.6f

--- projects/budova.partners/db/models/user.model.js
+++ projects/budova.partners/db/models/user.model.js
@@ -4,9 +4,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
 username: {type: String, unique: true},
-password: {type: String},
-firstname: String,
-lastname: String,
+password: {type: String}
 
 })
 
--- projects/budova.partners/routes/note.routes.js
+++ projects/budova.partners/routes/note.routes.js
@@ -17,7 +17,11 @@ function IsParseableJson(str) {
     return true;
 }
 
-
+function json_Result(string){
+  let obj = { result: string};
+  let myJSON = JSON.stringify(obj);
+  return myJSON;
+}
 function express_page(db_id, pageurl, ejs_file, app, dbmodel, express,  IsParseableJson){
   app.get(pageurl, function(req, res) {
       d = {};
@@ -48,44 +52,89 @@ module.exports = (app, express, bodyParser) => {
   var dbmodel = require('../db/models/note.model.js');
   var User = require('../db/models/user.model.js');
   const notes = require('../controllers/note.controller.js');
-
-
+  var session  = require('express-session');
+    app.use(session({secret:"f254fr45t43ty5409143t91y4ty920ty123", resave:false, saveUninitialized:true}))
 //LOGIN-REGISTER
-  app.post('/login', (req, res) => {
-    var username = req.body.username;
-    var password = req.body.password;
-    User.findOne({
-      username:username,
-      password:password,
-    }, function(req,res){
-        if (err) { console.log(err); return res.status(500).send() }
-        if (!user) { return res.status(404).send(); }
-      return res.status(200).send();
-    })
+
+  app.get('/dashboard', function(req, res){
+    if (!req.session.user) {
+      return res.status(401).send();
+    }
+    return res.status(200).send("Welcome to super-secret API")
   })
-  app.post('/register', (req, res) => {
-      d = {}
-      d.title  = 'loginCHECK_POST'
+
+  app.get('/enter', function(req, res){
+    // if (!req.session.user) {
+    //   return res.status(401).send();
+    // }
+    return res.status(200).render('login.ejs', {
+
+    });
+  })
+
+
+
+
+app.post('/login', function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+  User.findOne({
+    username:username,
+    password:password,
+  }, function(err,user){
+        if (err) {
+          let answer = 'Произошла ошибка';
+          let json_result = json_Result(answer)
+          return res.status(500).send(json_result)
+        }
+        if (!user) {
+          let answer = 'Не существует пользователя с именем ' + username + '!';
+          let json_result = json_Result(answer)
+          return res.status(404).send(json_result);
+        }
+        req.session.user =  user;
+        let answer = 'Добро пожаловать ' + username + '!';
+        let json_result = json_Result(answer)
+        return res.status(200).send(json_result);
+      })
+})
+
+
+
+
+
+
+
+
+  app.post('/register', function(req, res) {
       var username = req.body.username;
       var password = req.body.password;
-      var firstname = req.body.firstname;
-      var lastname = req.body.lastname;
       var newuser = new User();
       newuser.username = username;
       newuser.password = password;
-      newuser.firstname = firstname;
-      newuser.lastname = lastname;
-      newuser.save(function(err, savedUser){
-        if (err) { console.log(err); return res.status(500).send() }
-        return res.status(200).send()
-      })
-      res.status(200).render('login', { d: d });
+      User.findOne({
+        username:username,
+        password:password,
+      }, function(err,user){
+      if (!user) {
+        newuser.save(function(err, savedUser){ if (err) {  console.log(err);  } })
+            return res.status(200).send(json_Result('Поздравляем с успешной регистрацией, ' + username + '!'))
+      }
+      res.status(200).send(json_Result('Имя ' + username + ' уже занято!'));
+    })
+
   });
+
+
+
 //LOGIN-REGISTER
 
 
 
-
+app.post('/logout', function(req, res) {
+  req.session.destroy()
+  res.status(200).send()
+})
 
 
 
