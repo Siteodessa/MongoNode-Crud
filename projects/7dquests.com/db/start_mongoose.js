063e9770f8c0dfb9b948bece93125747abff4950
Siteodessa <siteodessa.com@gmail.com>
2018-10-25 21:32:28 +0300

    v0.0.6

--- projects/7dquests.com/db/start_mongoose.js
+++ projects/7dquests.com/db/start_mongoose.js
@@ -1,5 +1,6 @@
 module.exports = (mongoose, dbConfig) => {
   mongoose.Promise = global.Promise;
-  mongoose.connect(dbConfig.url) .then(() => {    console.log("db:ok");}) .catch(err => {    console.log('db:false');});
+  mongoose.connect(dbConfig.url,{ useNewUrlParser: true }) .then(() => {    console.log("db:ok");}) .catch(err => {    console.log('db:false');});
+  mongoose.set('useCreateIndex', true);
   //    process.exit();
 }
