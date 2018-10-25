160943d48cfa6c991ca010f4855b866806e11f49
Siteodessa <siteodessa.com@gmail.com>
2018-10-25 04:51:41 +0300

    v0.0.5e2

--- projects/7dquests.com/entrypoint.js
+++ projects/7dquests.com/entrypoint.js
@@ -1,6 +1,6 @@
 console.log('Starting...');
 var fs = require('fs');
-let express = require('../../node_modules/express') || require('../node_modules/express') || require('express');
+let express = require('express');
 let app = express();
 const dbConfig = require('./db/config/database.config.js');
 const mongoose = require('../../node_modules/mongoose');
